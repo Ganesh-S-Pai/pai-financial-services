835b48159ae1b551c14ba3df63d2ae45b93e40f8
Ganesh-S-Pai <sachin11496@gmail.com>
2025-10-01 10:54:37 +0530

    feat: Added Vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <v-card height="200" class="d-flex flex-column align-center justify-center">
+    <h1>You did it!</h1>
+    <p>
+      Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+      documentation
+    </p>
+  </v-card>
 </template>
 
 <style scoped></style>
