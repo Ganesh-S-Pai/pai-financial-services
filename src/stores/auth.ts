c57764b1bfb9a5513d2ce1dba0434f528f919781
Ganesh-S-Pai <sachin11496@gmail.com>
2025-10-06 22:46:10 +0530

    feat: Fixed build issue due to type mismatch with pinia-persisted-state

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -1,9 +1,16 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
-export const useAuthStore = defineStore('auth', () => {
-    const userId = ref<string>()
-    const token = ref<string>()
+export const useAuthStore = defineStore(
+    'useAuth',
+    () => {
+        const userId = ref<string>()
+        const token = ref<string>()
 
-    return { userId, token }
-}, { persist: true })
+        return { userId, token }
+    },
+    {
+        persist: true
+    } as any
+    // NOTE persist option is passed with type any as Typescript was throwing error due to defineStore not defining persist as a valid property
+)
